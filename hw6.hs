8f98739d6ca015d10739ba850516010e142f2d01
Yu Feng <fengyu8299@gmail.com>
2014-04-09 10:57:17 -0500

    finish basic requirement.

--- hw6.hs
+++ hw6.hs
@@ -86,14 +86,52 @@ checkError (x:xs) = if (fst x == TError) then
                         checkError xs
 --Type inference
 infer :: Exp -> Type
-infer e = let env = Map.empty
-            in let constr = (getConstraints e 1 env)
-                in let newType = thd3 constr
-                    in grep (unification (fst3 constr)) newType
-                    --in unification (fst3 constr) 
+infer e = let env = Map.empty in 
+          let constr = (getConstraints e 1 env) in 
+          let newType = thd3 constr in 
+              grep (unification (fst3 constr)) newType
+              --in unification (fst3 constr) 
+
+--infer' :: Exp -> Data.Map -> Type
+infer' e env = 
+            let constr = (getConstraints e 1 env) in 
+            let newType = thd3 constr in 
+                grep (unification (fst3 constr)) newType
 
 eliminate :: Maybe Type -> Type 
 eliminate (Just a) = a
+eliminate _  = TError
+
+instantiate :: Type -> Int -> (Type,Int)
+instantiate (TPoly arr t) f =
+    case arr of
+        [] -> (TVar "inst", f)
+        (x:xs) -> 
+            let instList = buildInstList arr f  in
+            (getType instList t, f)
+
+solveS :: [(String, Type)] -> String -> Type
+solveS [] ty = TVar "SOLVE"
+solveS (x:xs) newType = 
+                      if (fst x == newType) then 
+                          snd x
+                      else 
+                          solveS xs newType
+ 
+getType :: [(String, Type)] -> Type -> Type
+getType instList t =
+    case t of
+        --TVar x -> solveS instList x 
+        TVar x -> 
+            let newlist = (Map.fromList instList) in 
+            eliminate (Map.lookup x $ newlist)
+        TFun x y -> TFun (getType instList x) (getType instList y)
+
+buildInstList :: [String] -> Int -> [(String, Type)]
+buildInstList args f =
+    case args of
+        [] -> []
+        (x:xs) -> [(x,TVar ("x"++ show (f+100)))] ++ (buildInstList xs (f+1001))
 
 --extract constraints
 --getConstraints :: Exp -> [(Type, Type)]
@@ -107,12 +145,23 @@ getConstraints (Literal (BoolV x)) f env = let new = TVar ("x"++ show f)
 getConstraints (Variable  x) f env = 
             if Map.member x $ env then
             --get the value from env.
-                let tx = eliminate (Map.lookup x $ env)
-                in  ([], f, tx)
+                let tx = eliminate (Map.lookup x $ env) in  
+                    case tx of
+                        --if it's polymorphism, instantiate it with fresh var..
+                        TPoly arr t -> 
+                              case t of
+                                TVar y -> 
+                                    --let new = TVar ("x"++ show f) in
+                                    let (new,f1) = instantiate (TPoly arr t) f in 
+                                    ([(new, TVar ("x"++ show (f+1))) ], f, new)
+                                TFun y1 y2 ->
+                                    --let new = TVar ("x"++ show f) in
+                                    let (new,f1) = instantiate (TPoly arr t) f in 
+                                    ([(new, TVar ("x"++ show (f+1))) ], f, new)
+                        _ -> ([], f, tx)
             else
             --create a new one.
                 ([], f, (TVar("x"++ show f)))
-            --polymorphism....
 
 getConstraints (Binary Add e1 e2) f env = let new = TVar ("x"++ show f) in
                              let (c1, f1, t1) = getConstraints e1 (f+1) env in
@@ -124,9 +173,30 @@ getConstraints (Binary Mul e1 e2) f env = let new = TVar ("x"++ show f) in
                              let (c2, f2, t2) = getConstraints e2 (f1+1) env in
                              ([(new, TInt), (t1,TInt), (t2,TInt)] ++ c1 ++ c2, f, new)
 
-getConstraints (Binary EQ e1 e2) f env = let new = TVar ("x"++ show f) in
+--              | GT | LT | LE | GE | EQ
+getConstraints (Binary GT e1 e2) f env = let new = TVar ("x-gt"++ show f) in
                             let (c1, f1, t1) = getConstraints e1 (f+1) env in
                             let (c2, f2, t2) = getConstraints e2 (f1+1) env in
+                            ([(new, TBool), (t1,t2)] ++ c1 ++ c2, f, new)
+
+getConstraints (Binary LT e1 e2) f env = let new = TVar ("x"++ show f) in
+                            let (c1, f1, t1) = getConstraints e1 (f+1) env in
+                            let (c2, f2, t2) = getConstraints e2 (f1+1) env in
+                            ([(new, TBool), (t1,t2)] ++ c1 ++ c2, f, new)
+
+getConstraints (Binary LE e1 e2) f env = let new = TVar ("x"++ show f) in
+                            let (c1, f1, t1) = getConstraints e1 (f+1) env in
+                            let (c2, f2, t2) = getConstraints e2 (f1+1) env in
+                            ([(new, TBool), (t1,t2)] ++ c1 ++ c2, f, new)
+
+getConstraints (Binary GE e1 e2) f env = let new = TVar ("x"++ show f) in
+                            let (c1, f1, t1) = getConstraints e1 (f+1) env in
+                            let (c2, f2, t2) = getConstraints e2 (f1+1) env in
+                            ([(new, TBool), (t1,t2)] ++ c1 ++ c2, f, new)
+
+getConstraints (Binary EQ e1 e2) f env = let new = TVar ("x-eq"++ show f) in
+                            let (c1, f1, t1) = getConstraints e1 (f+1) env in
+                            let (c2, f2, t2) = getConstraints e2 (f1+770) env in
                             ([(new, TBool), (t1,t2)] ++ c1 ++ c2, f, new)
 
 
@@ -141,16 +211,38 @@ getConstraints (Function x body) f env = let tme = TVar ("x"++ show f) in
  --                           let (c2, f2, t2) = getConstraints e2 (f1+1) in
  --                           ([(new, t12), (t11,t2)] ++ c1 ++ c2, f, new)
 
-getConstraints (Call e1 e2) f env = let new = TVar ("x"++ show f) in
+{-getConstraints (Call e1 e2) f env = let new = TVar ("x"++ show f) in
                             let (c1, f1, t1) = getConstraints e1 (f+1) env in
                             let (c2, f2, t2) = getConstraints e2 (f1+1) env in
-                            ([(t1, (TFun t2 new))] ++ c1 ++ c2, f, new)
+                            ([(t1, (TFun t2 new))] ++ c1 ++ c2, f, new) -}
+
+getConstraints (Call e1 e2) f env = 
+        case e1 of
+            Variable x ->
+                        let new = TVar ("x-call"++ show f) in
+                        let (c1, f1, (TFun targ tbody)) = getConstraints e1 (f+1) env in
+                        let (c2, f2, t2) = getConstraints e2 (f1+1) env in
+                        --([(t1, (TFun t2 new))] ++ c1 ++ c2, f, new)
+                        ([(targ, t2), (new, tbody)] ++ c2, f, new)
+            _ ->
+                        let new = TVar ("xxx-call"++ show f) in
+                        let (c1, f1, t1) = getConstraints e1 (f+1) env in
+                        let (c2, f2, t2) = getConstraints e2 (f1+1) env in
+                        ([(t1, (TFun t2 new))] ++ c1 ++ c2, f, new)
 
 
-getConstraints (Declare str e1 e2) f env = let new = TVar ("x"++ show f) in
-                            let (c1, f1, t1) = getConstraints e1 (f+1) env in
-                            let (c2, f2, t2) = getConstraints e2 (f1+1) env in
-                            ([(new, t2)] ++ c1 ++ c2, f, new)
+{-getConstraints (Declare id exp body) f env = 
+                            let new = TVar ("x"++ show f) in
+                            let (c1, f1, t1) = getConstraints exp (f+1) env in
+                            let (c2, f2, t2) = getConstraints body (f1+1) env in
+                            ([(new, t2)] ++ c1 ++ c2, f, new)-}
+
+getConstraints (Declare id exp body) f env = 
+                            let texp = infer' exp env in
+                            let tgen = generalize texp in
+                            let newenv =  Map.insert id tgen env in
+                            getConstraints body f newenv
+
 
 getConstraints (If e1 e2 e3) f env = let new = TVar ("x"++ show f) in
                             let (c1, f1, t1) = getConstraints e1 (f+1) env in
@@ -158,6 +250,30 @@ getConstraints (If e1 e2 e3) f env = let new = TVar ("x"++ show f) in
                             let (c3, f3, t3) = getConstraints e3 (f2+1) env in
                             ([(t1, TBool), (t2,t3)] ++ c1 ++ c2 ++ c3, f, new)
 
+--getConstraints for let-polymorphism
+{-getC (Declare id exp body) f env = 
+                            let texp = infer' exp env in
+                            let tgen = generalize texp in
+                            let newenv =  Map.insert id tgen env in
+                            getConstraints body f newenv-}
+
+--let-polymorphism generalize TPoly
+--generalize texp =  TPoly [String] Type
+generalize texp =  
+                case texp of
+                    TInt -> TInt
+                    TBool-> TBool
+                    TVar x ->  TPoly [x] (TVar x)
+                    TFun t1 t2 -> TPoly (nub (extractVar t1 ++ extractVar t2))  (TFun t1 t2)
+
+--extract Variable(s) from Type.
+extractVar texp =
+                case texp of
+                    TInt -> []
+                    TBool-> []
+                    TVar x -> [x]
+                    --remove duplicated elements
+                    TFun t1 t2 -> nub ((extractVar t1) ++ (extractVar t2))
 
 replace x e1 (Literal a) = Literal a
 replace x e1 (Variable y) = if x == y then
@@ -249,7 +365,7 @@ solve (x:xs) newType =
                       if (fst x == newType) then 
                           snd x
                       else 
-                          grep xs newType
+                          solve xs newType
  
 {-
 compose l (tx, ty) =
@@ -267,10 +383,6 @@ swap tx (ty1, ty2) = if tx == ty1 then
                          ty2
                      else 
                          tx
-
---let-polymorphism
---generalize l =
-
 
 {-
 let applysubst constr tyT =
